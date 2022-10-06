2836194648af905b317a4bb605545c0081baec89
Yevhen <epsonn537@gmail.com>
2022-10-06 03:37:45 +0300

    s

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Container from '../../components/Container/Container'
 
 const Product: React.FC = () => {
     const { product } = useParams()
 
     return (
-        <div>{product}</div>
+        <div className='w-full flex justify-center'>
+            <Container>
+                <h1 className='text-3xl font-medium text-center'>{product}</h1>
+            </Container>
+        </div>
     )
 }
 
